612c99cd3d0ae31ce8ce3c40c2c9b6483a8c82bf
dopgima <elvis@p1sec.com>
2011-11-02 09:58:04 +0100

    src/sipcontrib/: packaging sip helpers

--- src/core/setup.py
+++ src/core/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-NAME='engine'
-NAME='engine'
+NAME='core'
+NAME='core'
 VERSION='0.1.0'
 LICENSE='UNDECIDED'
 MAINTAINER=AUTHOR='d0hm4t06 3. d0p91m4'
 MAINTAINER_EMAIL=AUTHOR_EMAIL='gmdopp@gmail.com'
-PACKAGES=['engine',]
-PACKAGE_DIR={'engine':'.'}
+PACKAGES=['core',]
+PACKAGE_DIR={'core':'.'}
 
 setup(name=NAME,
       version=VERSION,
---
+++ src/sipcontrib/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+NAME='sipcontrib'
+NAME='sipcontrib'
+VERSION='0.1.0'
+LICENSE='UNDECIDED'
+MAINTAINER=AUTHOR='d0hm4t06 3. d0p91m4'
+MAINTAINER_EMAIL=AUTHOR_EMAIL='gmdopp@gmail.com'
+PACKAGES=['sipcontrib',]
+PACKAGE_DIR={'sipcontrib':'.'}
+
+setup(name=NAME,
+      version=VERSION,
+      license=LICENSE,
+      maintainer=MAINTAINER,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      author_email=AUTHOR_EMAIL,
+      packages=PACKAGES,
+      package_dir=PACKAGE_DIR,
+      )
